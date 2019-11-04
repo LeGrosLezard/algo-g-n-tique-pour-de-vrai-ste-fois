b9cdaa1b4f0968a9ce25168b9f59fcfdc279aac2
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-04 23:45:28 +0100

    Update parcourage.py

--- parcourage.py
+++ parcourage.py
@@ -35,8 +35,9 @@ def recup_contours(img):
 
 
 
-def lign_contour():
-    pass
+
+
+
 
 
 
@@ -62,31 +63,7 @@ for y in range(blanck.shape[1]):
 x = pointsx[0][0]
 y = pointsx[0][1]
 
-xz = pointsx[0][0]
-yz = pointsx[0][1]
 
-xu = pointsx[0][0]
-yu = pointsx[0][1]
-
-
-xd = pointsx[0][0]
-yd = pointsx[0][1]
-
-xt = pointsx[0][0]
-yt = pointsx[0][1]
-
-
-xq = pointsx[0][0]
-yq = pointsx[0][1]
-
-xc = pointsx[0][0]
-yc = pointsx[0][1]
-
-xs = pointsx[0][0]
-ys = pointsx[0][1]
-
-xse = pointsx[0][0]
-yse = pointsx[0][1]
 #cv2.circle(copy, (x, y), 3, (255, 0, 0), 3)
 
 #copy[x, y] = 0, 0, 255
@@ -98,36 +75,62 @@ yse = pointsx[0][1]
 i = 1
 
 
-#--------------------
+def lign_contour(parcourage):
+    if parcourage[-2] == parcourage[-1]:
+        print("lign")
+        print(parcourage)
+        print("")
 
-"""
-éclaireur -> avant poste - > 'constructeur de route' -> eclaireur
 
-"""
+        
+        
 
-#--------------------
+def escalier(parcourage):
+    if parcourage[-4] == parcourage[-3] and\
+       parcourage[-2] == 1 and parcourage[-1] == 1 and\
+       parcourage[-3] != parcourage[-2]:
+        print("escalier descendant")
+        print(parcourage)
+        print("")
+
+
+    if parcourage[-4] == parcourage[-3] and\
+       parcourage[-2] == 0 and parcourage[-1] == 0 and\
+       parcourage[-3] != parcourage[-2]:
+        print("escalier qui monte")
+        print(parcourage)
+        print("")
+
+
+def coin(parcourage):
+    if parcourage[-3] == parcourage[-2] and\
+        parcourage[-1] == 0 and\
+        parcourage[-2] != 0:
+        print("coin gauche")
+        print(parcourage)
+        print("")
+
+    if parcourage[-3] == parcourage[-2] and\
+        parcourage[-1] == 1 and\
+        parcourage[-2] != 1:
+        print("coin droit")
+        print(parcourage)
+        print("")
 
 
 
 poste = []
 copy1 = copy.copy()
-copy_zero = copy.copy()
-copy_un = copy.copy()
-copy_deux = copy.copy()
-copy_trois = copy.copy()
-copy_quattre = copy.copy()
-copy_cinq = copy.copy()
-copy_six = copy.copy()
-copy_sept = copy.copy()
-
 
 lastx = 0
 lasty = 0
 
-while True:
-    #copy1 = copy.copy()
-    
 
+parcourage = []
+
+while True:
+
+    
     print(poste)
 
     if poste != []:
@@ -141,7 +144,15 @@ while True:
             if post == 0:
                 print("")
                 print("poste 0")
+                parcourage.append(0)
 
+                try:
+                    lign_contour(parcourage)
+                    escalier(parcourage)
+                    coin(parcourage)
+                except:
+                    pass
+            
 
                 copy1[lastx + 1, lasty] = 0, 0, 255
                 blanck[lastx + 1, lasty] = 255
@@ -149,6 +160,7 @@ while True:
 
                 xz = lastx + 1
                 yz = lasty
+
                 eclaireurs = [blanck[xz + 1, yz], blanck[xz - 1, yz], 
                               blanck[xz, yz + 1], blanck[xz, yz - 1],
                               blanck[xz + 1, yz + 1], blanck[xz - 1, yz - 1],
@@ -181,20 +193,78 @@ while True:
                         lastx = xz
                         lasty = yz
 
-                #et a leur tour d'envoyer des éclaireurs
-##                copy1[xz + 1, yz] = 0, 0, 255
-##                copy1[xz - 1, yz] = 0, 0, 255
-##                copy1[xz, yz + 1] = 0, 0, 255
-##                copy1[xz, yz - 1] = 0, 0, 255
-##                copy1[xz + 1, yz + 1] = 0, 0, 255
-##                copy1[xz - 1, yz - 1] = 0, 0, 255
-##                copy1[xz + 1, yz - 1] = 0, 0, 255
-##                copy1[xz - 1, yz + 1] = 0, 0, 255
+
+                show_picture("copy1", copy1, 0, "")
+
+
+
+
+
+
+
+
+
+            if post == 1:
+                print("")
+                print("poste 1")
+                parcourage.append(1)
+
+                try:
+                    lign_contour(parcourage)
+                    escalier(parcourage)
+                    coin(parcourage)
+                except:
+                    pass
+
+
+
+
+
+
+                copy1[lastx, lasty] = 255, 0, 0
+                print("iciiiiiiiiiiiiiiiiiiiii")
+                copy1[lastx - 1, lasty] = 0, 0, 255
+                blanck[lastx - 1, lasty] = 255
+
+                cv2.imwrite("ICI.png", copy1)
+
+                
+                xu = lastx - 1
+                yu = lasty
+
+                eclaireurs = [blanck[xu + 1, yu], blanck[xu - 1, yu], 
+                              blanck[xu, yu + 1], blanck[xu, yu - 1],
+                              blanck[xu + 1, yu + 1], blanck[xu - 1, yu - 1],
+                              blanck[xu + 1, yu - 1], blanck[xu - 1, yu + 1]]
+
+                poste = []
+
+
+
+                #Routes
+                for nb, eclaireur in enumerate(eclaireurs):
+                    if eclaireur == 255:
+                        print(eclaireur, nb)
+
+                        if nb == 0:pass
+                        elif nb == 1:poste.append(1)
+                        elif nb == 2:poste.append(2)
+                        elif nb == 3:poste.append(3)
+                        elif nb == 4:poste.append(4)
+                        elif nb == 5:poste.append(5)
+                        elif nb == 6:poste.append(6)
+                        elif nb == 7:poste.append(7)
+
+
+                        lastx = xu
+                        lasty = yu
 
 
                 show_picture("copy1", copy1, 0, "")
-                cv2.imwrite("coco.png",copy1)
-                cv2.imwrite("dza444dzadza.png",blanck)
+
+
+
+
 
 
 
@@ -204,6 +274,14 @@ while True:
             if post == 2:
                 print("")
                 print("poste 2")
+                parcourage.append(2)
+
+                try:
+                    lign_contour(parcourage)
+                    escalier(parcourage)
+                    coin(parcourage)
+                except:
+                    pass
 
                 copy1[lastx, lasty+1] = 0, 0, 255
                 blanck[lastx, lasty+1] = 255
@@ -228,27 +306,186 @@ while True:
 
                         if nb == 0:poste.append(0)
                         elif nb == 1:poste.append(1)
-                        elif nb == 2:
-                            poste.append(2)
+                        elif nb == 2:poste.append(2)
                         elif nb == 3:pass
-                        elif nb == 4:
-                            poste.append(4)
-                        elif nb == 5:
-                            poste.append(5)
-                        elif nb == 6:
-                            poste.append(6)
-                        elif nb == 7:
-                            poste.append(7)
+                        elif nb == 4:poste.append(4)
+                        elif nb == 5:poste.append(5)
+                        elif nb == 6:poste.append(6)
+                        elif nb == 7:poste.append(7)
 
 
                         lastx = xd
                         lasty = yd
 
-                cv2.imwrite("coco.png",copy1)
-                cv2.imwrite("dzadzadza.png",blanck)
+                #cv2.imwrite("ici.png", copy1)
                 show_picture("copy1", copy1, 0, "")
 
 
+
+
+
+
+
+
+
+            if post == 3:
+                print("")
+                print("poste 3")
+                parcourage.append(3)
+
+
+                try:
+                    lign_contour(parcourage)
+                    escalier(parcourage)
+                    coin(parcourage)
+                except:
+                    pass
+
+                copy1[lastx, lasty - 1] = 0, 0, 255
+                blanck[lastx, lasty - 1] = 255
+
+                xt = lastx
+                yt = lasty - 1
+
+
+                eclaireurs = [blanck[xt + 1, yt], blanck[xt - 1, yt], 
+                              blanck[xt, yt + 1], blanck[xt, yt - 1],
+                              blanck[xt + 1, yt + 1], blanck[xt - 1, yt - 1],
+                              blanck[xt + 1, yt - 1], blanck[xt - 1, yt + 1]]
+
+                poste = []
+
+
+                #Routes
+                for nb, eclaireur in enumerate(eclaireurs):
+                    if eclaireur == 255:
+                        print(eclaireur, nb)
+
+                        if nb == 0:poste.append(0)
+                        elif nb == 1:poste.append(1)
+                        elif nb == 2:pass
+                        elif nb == 3:poste.append(3)
+                        elif nb == 4:poste.append(4)
+                        elif nb == 5:poste.append(5)
+                        elif nb == 6:poste.append(6)
+                        elif nb == 7:poste.append(7)
+
+
+                        lastx = xt
+                        lasty = yt
+
+
+                show_picture("copy1", copy1, 0, "")
+
+
+
+
+
+
+
+
+
+
+            if post == 6:
+                print("")
+                print("poste 6")
+                parcourage.append(6)
+
+                try:
+                    lign_contour(parcourage)
+                    escalier(parcourage)
+                    coin(parcourage)
+                except:
+                    pass
+
+
+                copy1[lastx - 1, lasty - 1] = 0, 0, 255
+                blanck[lastx - 1, lasty - 1] = 255
+
+
+                xs = lastx - 1
+                ys = lasty - 1
+                
+                eclaireurs = [blanck[xs + 1, ys], blanck[xs - 1, y], 
+                              blanck[xs, ys + 1], blanck[xs, y - 1],
+                              blanck[xs + 1, ys + 1], blanck[xs - 1, ys - 1],
+                              blanck[xs + 1, ys - 1], blanck[xs - 1, ys + 1]]
+
+                poste = []
+
+
+
+
+
+                #Routes
+                for nb, eclaireur in enumerate(eclaireurs):
+                    if eclaireur == 255:
+                        print(eclaireur, nb)
+
+                        if nb == 0:poste.append(0)
+                        elif nb == 1:poste.append(1)
+                        elif nb == 2:poste.append(2)
+                        elif nb == 3:poste.append(3)
+                        elif nb == 4:poste.append(4)
+                        elif nb == 5:poste.append(5)
+                        elif nb == 6:poste.append(6)
+                        elif nb == 7:poste.append(7)
+
+
+                        lastx = xs
+                        lasty = ys
+
+                cv2.imwrite("ici.png", copy1)
+                show_picture("copy1", copy1, 0, "")
+
+
+
+
+
+
+
+
+
+
+
+
+##            if post == 7:
+##                print("poste 7")
+##
+##                copy1[lastx - 1, lasty + 1] = 0, 0, 255
+##                blanck[lastx - 1, lasty + 1] = 255
+##
+##                xs = lastx - 1
+##                ys = lasty + 1
+##
+##
+##                eclaireurs = [blanck[xse + 1, yse], blanck[xse - 1, yse], 
+##                              blanck[xse, yse + 1], blanck[xse, yse - 1],
+##                              blanck[xse + 1, yse + 1], blanck[xse - 1, yse - 1],
+##                              blanck[xse + 1, yse - 1], blanck[xse - 1, yse + 1]]
+##
+##                poste = []
+##
+##
+##                #Routes
+##                for nb, eclaireur in enumerate(eclaireurs):
+##                    if eclaireur == 255:
+##                        print(eclaireur, nb)
+##
+##                        if nb == 0:poste.append(0)
+##                        elif nb == 1:poste.append(1)
+##                        elif nb == 2:poste.append(2)
+##                        elif nb == 3:poste.append(3)
+##                        elif nb == 4:poste.append(4)
+##                        elif nb == 5:poste.append(5)
+##                        elif nb == 6:poste.append(6)
+##                        elif nb == 7:poste.append(7)
+##
+##                        lastx = xse
+##                        lasty = yse
+##
+##
+##                show_picture("copy1", copy1, 0, "")
 
 
 
@@ -319,47 +556,5 @@ while True:
         #show_picture("copy1", copy1, 0, "")
 
         
-
-
-
-
-
-
-
-##    #Lign
-##    lign = 0
-##    while True:
-##        if blanck[x + lign, y] == 0:
-##            break
-##        else:
-##            print(blanck[x + lign, y])
-##            copy[x + lign, y] = 0, 0, 255
-##            lign += 1
-##            show_picture("copy", copy, 0, "")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
