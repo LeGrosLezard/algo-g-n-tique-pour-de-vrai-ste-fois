daf8ead0a3ad863ef90ed945a7473463fdc2e965
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-04 21:06:22 +0100

    Update parcourage.py
    pcqu'on veut que ca le fasse en meme temps, pas grave apres

--- parcourage.py
+++ parcourage.py
@@ -119,11 +119,16 @@ copy_quattre = copy.copy()
 copy_cinq = copy.copy()
 copy_six = copy.copy()
 copy_sept = copy.copy()
+
+
+lastx = 0
+lasty = 0
+
 while True:
     #copy1 = copy.copy()
     
 
-    print(poste, x, y, i)
+    print(poste)
 
     if poste != []:
         for post in poste:
@@ -134,11 +139,16 @@ while True:
 
 
             if post == 0:
+                print("")
                 print("poste 0")
 
-                
-                xz = xz+1
 
+                copy1[lastx + 1, lasty] = 0, 0, 255
+                blanck[lastx + 1, lasty] = 255
+
+
+                xz = lastx + 1
+                yz = lasty
                 eclaireurs = [blanck[xz + 1, yz], blanck[xz - 1, yz], 
                               blanck[xz, yz + 1], blanck[xz, yz - 1],
                               blanck[xz + 1, yz + 1], blanck[xz - 1, yz - 1],
@@ -146,113 +156,45 @@ while True:
 
                 poste = []
 
-                #et a leur tour d'envoyer des éclaireurs
-                copy_zero[xz + 1, yz] = 0, 0, 255
-                copy_zero[xz - 1, yz] = 0, 0, 255
-                copy_zero[xz, yz + 1] = 0, 0, 255
-                copy_zero[xz, yz - 1] = 0, 0, 255
-                copy_zero[xz + 1, yz + 1] = 0, 0, 255
-                copy_zero[xz - 1, yz - 1] = 0, 0, 255
-                copy_zero[xz + 1, yz - 1] = 0, 0, 255
-                copy_zero[xz - 1, yz + 1] = 0, 0, 255
-
-
-
-
 
                 #Routes
                 for nb, eclaireur in enumerate(eclaireurs):
                     if eclaireur == 255:
                         print(eclaireur, nb)
 
-                        if nb == 0:poste.append(0)
-                        elif nb == 1:poste.append(1)
-                        elif nb == 2:poste.append(2)
+                        if nb == 0:
+                            poste.append(0)
+                        elif nb == 1:pass
+                        elif nb == 2:
+                            poste.append(2)
                         elif nb == 3:poste.append(3)
                         elif nb == 4:
-                            blanck[xz+1,yz] = 255
                             poste.append(4)
                         elif nb == 5:
-                            blanck[xz-1,yz] = 255
                             poste.append(5)
                         elif nb == 6:
-                            blanck[xz+1,yz] = 255
                             poste.append(6)
                         elif nb == 7:
-                            blanck[xz-1, yz] = 255
                             poste.append(7)
 
 
-
-
-
-                show_picture("copy_zero", copy_zero, 0, "")
-
-
-
-
-
-
-
-
-            if post == 1:
-                print("poste 1")
-
-
-                xu = xu-1
-                yu = yu
-                
-                eclaireurs = [blanck[xu + 1, yu], blanck[xu - 1, yu], 
-                              blanck[xu, yu + 1], blanck[xu, yu - 1],
-                              blanck[xu + 1, yu + 1], blanck[xu - 1, yu - 1],
-                              blanck[xu + 1, yu - 1], blanck[xu - 1, yu + 1]]
-
-                poste = []
+                        lastx = xz
+                        lasty = yz
 
                 #et a leur tour d'envoyer des éclaireurs
-                copy_un[xu + 1, yu] = 0, 0, 255
-                copy_un[xu - 1, yu] = 0, 0, 255
-                copy_un[xu, yu + 1] = 0, 0, 255
-                copy_un[xu, yu - 1] = 0, 0, 255
-                copy_un[xu + 1, yu + 1] = 0, 0, 255
-                copy_un[xu - 1, yu - 1] = 0, 0, 255
-                copy_un[xu + 1, yu - 1] = 0, 0, 255
-                copy_un[xu - 1, yu + 1] = 0, 0, 255
+##                copy1[xz + 1, yz] = 0, 0, 255
+##                copy1[xz - 1, yz] = 0, 0, 255
+##                copy1[xz, yz + 1] = 0, 0, 255
+##                copy1[xz, yz - 1] = 0, 0, 255
+##                copy1[xz + 1, yz + 1] = 0, 0, 255
+##                copy1[xz - 1, yz - 1] = 0, 0, 255
+##                copy1[xz + 1, yz - 1] = 0, 0, 255
+##                copy1[xz - 1, yz + 1] = 0, 0, 255
 
 
-
-                #Routes
-                for nb, eclaireur in enumerate(eclaireurs):
-                    if eclaireur == 255:
-                        print(eclaireur, nb)
-
-                        if nb == 0:poste.append(0)
-                        elif nb == 1:poste.append(1)
-                        elif nb == 2:poste.append(2)
-                        elif nb == 3:poste.append(3)
-                        elif nb == 4:
-                            blanck[xu+1,yu] = 255
-                            poste.append(4)
-                        elif nb == 5:
-                            blanck[xu-1,yu] = 255
-                            poste.append(5)
-                        elif nb == 6:
-                            blanck[xu+1,yu] = 255
-                            poste.append(6)
-                        elif nb == 7:
-                            blanck[xu-1, yu] = 255
-                            poste.append(7)
-
-
-
-
-
-                show_picture("copy_un", copy_un, 0, "")
-
-
-
-
-
+                show_picture("copy1", copy1, 0, "")
+                cv2.imwrite("coco.png",copy1)
+                cv2.imwrite("dza444dzadza.png",blanck)
 
 
 
@@ -260,11 +202,16 @@ while True:
 
 
             if post == 2:
+                print("")
                 print("poste 2")
 
-                xd = xd
-                yd = yd+1
-                
+                copy1[lastx, lasty+1] = 0, 0, 255
+                blanck[lastx, lasty+1] = 255
+
+                xd = lastx
+                yd = lasty + 1
+
+
                 eclaireurs = [blanck[xd + 1, yd], blanck[xd - 1, yd], 
                               blanck[xd, yd + 1], blanck[xd, yd - 1],
                               blanck[xd + 1, yd + 1], blanck[xd - 1, yd - 1],
@@ -272,16 +219,6 @@ while True:
 
                 poste = []
 
-                #et a leur tour d'envoyer des éclaireurs
-                copy_deux[xd + 1, yd] = 0, 0, 255
-                copy_deux[xd - 1, yd] = 0, 0, 255
-                copy_deux[xd, yd + 1] = 0, 0, 255
-                copy_deux[xd, yd - 1] = 0, 0, 255
-                copy_deux[xd + 1, yd + 1] = 0, 0, 255
-                copy_deux[xd - 1, yd - 1] = 0, 0, 255
-                copy_deux[xd + 1, yd - 1] = 0, 0, 255
-                copy_deux[xd - 1, yd + 1] = 0, 0, 255
-
 
 
                 #Routes
@@ -291,339 +228,25 @@ while True:
 
                         if nb == 0:poste.append(0)
                         elif nb == 1:poste.append(1)
-                        elif nb == 2:poste.append(2)
-                        elif nb == 3:poste.append(3)
+                        elif nb == 2:
+                            poste.append(2)
+                        elif nb == 3:pass
                         elif nb == 4:
-                            blanck[xd+1,yd] = 255
                             poste.append(4)
                         elif nb == 5:
-                            blanck[xd-1,yd] = 255
                             poste.append(5)
                         elif nb == 6:
-                            blanck[xd+1,yd] = 255
                             poste.append(6)
                         elif nb == 7:
-                            blanck[xd-1, yd] = 255
                             poste.append(7)
 
 
-
-
-
-                show_picture("copy_deux", copy_deux, 0, "")
-
-
-
-
-
-
-
-            if post == 3:
-                print("poste 3")
-
-                xt = xt
-                yt = yt-1
-                
-                eclaireurs = [blanck[xt + 1, yt], blanck[xt - 1, yt], 
-                              blanck[xt, yt + 1], blanck[xt, yt - 1],
-                              blanck[xt + 1, yt + 1], blanck[xt - 1, yt - 1],
-                              blanck[xt + 1, yt - 1], blanck[xt - 1, yt + 1]]
-
-                poste = []
-
-                #et a leur tour d'envoyer des éclaireurs
-                copy_trois[xt + 1, yt] = 0, 0, 255
-                copy_trois[xt - 1, yt] = 0, 0, 255
-                copy_trois[xt, yt + 1] = 0, 0, 255
-                copy_trois[xt, yt - 1] = 0, 0, 255
-                copy_trois[xt + 1, yt + 1] = 0, 0, 255
-                copy_trois[xt - 1, yt - 1] = 0, 0, 255
-                copy_trois[xt + 1, yt - 1] = 0, 0, 255
-                copy_trois[xt - 1, yt + 1] = 0, 0, 255
-
-
-
-                #Routes
-                for nb, eclaireur in enumerate(eclaireurs):
-                    if eclaireur == 255:
-                        print(eclaireur, nb)
-
-                        if nb == 0:poste.append(0)
-                        elif nb == 1:poste.append(1)
-                        elif nb == 2:poste.append(2)
-                        elif nb == 3:poste.append(3)
-                        elif nb == 4:
-                            blanck[xt+1,yt] = 255
-                            poste.append(4)
-                        elif nb == 5:
-                            blanck[xt-1,yt] = 255
-                            poste.append(5)
-                        elif nb == 6:
-                            blanck[xt+1,yt] = 255
-                            poste.append(6)
-                        elif nb == 7:
-                            blanck[xt-1, yt] = 255
-                            poste.append(7)
-
-
-
-
-
-                show_picture("copy_trois", copy_trois, 0, "")
-
-
-
-
-
-
-            if post == 4:
-                print("poste 4")
-
-
-                xq = xq + 1
-                yq = yq + 1
-  
-                eclaireurs = [blanck[xq + 1, yq], blanck[xq - 1, yq], 
-                              blanck[xq, yq + 1], blanck[xq, yq - 1],
-                              blanck[xq + 1, yq + 1], blanck[xq - 1, yq - 1],
-                              blanck[xq + 1, yq - 1], blanck[xq - 1, yq + 1]]
-
-                poste = []
-
-                #et a leur tour d'envoyer des éclaireurs
-                copy_quattre[xq + 1, yq] = 0, 0, 255
-                copy_quattre[xq - 1, yq] = 0, 0, 255
-                copy_quattre[xq, yq + 1] = 0, 0, 255
-                copy_quattre[xq, yq - 1] = 0, 0, 255
-                copy_quattre[xq + 1, yq + 1] = 0, 0, 255
-                copy_quattre[xq - 1, yq - 1] = 0, 0, 255
-                copy_quattre[xq + 1, yq - 1] = 0, 0, 255
-                copy_quattre[xq - 1, yq + 1] = 0, 0, 255
-
-
-
-                #Routes
-                for nb, eclaireur in enumerate(eclaireurs):
-                    if eclaireur == 255:
-                        print(eclaireur, nb)
-
-                        if nb == 0:poste.append(0)
-                        elif nb == 1:poste.append(1)
-                        elif nb == 2:poste.append(2)
-                        elif nb == 3:poste.append(3)
-                        elif nb == 4:
-                            blanck[xq+1,yq] = 255
-                            poste.append(4)
-                        elif nb == 5:
-                            blanck[xq-1,yq] = 255
-                            poste.append(5)
-                        elif nb == 6:
-                            blanck[xq+1,yq] = 255
-                            poste.append(6)
-                        elif nb == 7:
-                            blanck[xq-1, yq] = 255
-                            poste.append(7)
-
-
-
-
-
-                show_picture("copy_quattre", copy_quattre, 0, "")
-
-
-
-
-
-
-
-            if post == 5:
-                print("poste 5")
-
-                xc = xc + 1
-                yc = yc + 1
-                
-                eclaireurs = [blanck[xc + 1, yc], blanck[xc - 1, yc], 
-                              blanck[xc, yc + 1], blanck[xc, yc - 1],
-                              blanck[xc + 1, yc + 1], blanck[xc - 1, yc - 1],
-                              blanck[xc + 1, yc - 1], blanck[xc - 1, yc + 1]]
-
-                poste = []
-
-                #et a leur tour d'envoyer des éclaireurs
-                copy_cinq[xc + 1, yc] = 0, 0, 255
-                copy_cinq[xc - 1, yc] = 0, 0, 255
-                copy_cinq[xc, yc + 1] = 0, 0, 255
-                copy_cinq[xc, yc - 1] = 0, 0, 255
-                copy_cinq[xc + 1, yc + 1] = 0, 0, 255
-                copy_cinq[xc - 1, yc - 1] = 0, 0, 255
-                copy_cinq[xc + 1, yc - 1] = 0, 0, 255
-                copy_cinq[xc - 1, yc + 1] = 0, 0, 255
-
-
-
-                #Routes
-                for nb, eclaireur in enumerate(eclaireurs):
-                    if eclaireur == 255:
-                        print(eclaireur, nb)
-
-                        if nb == 0:poste.append(0)
-                        elif nb == 1:poste.append(1)
-                        elif nb == 2:poste.append(2)
-                        elif nb == 3:poste.append(3)
-                        elif nb == 4:
-                            blanck[xc+1,yc] = 255
-                            poste.append(4)
-                        elif nb == 5:
-                            blanck[xc-1,yc] = 255
-                            poste.append(5)
-                        elif nb == 6:
-                            blanck[xc+1,yc] = 255
-                            poste.append(6)
-                        elif nb == 7:
-                            blanck[xc-1, yc] = 255
-                            poste.append(7)
-
-
-
-
-
-                show_picture("copy_cinq", copy_cinq, 0, "")
-
-
-
-
-
-
-
-
-
-
-
-
-            if post == 6:
-                print("poste 6")
-
-
-                xs = xs - 1
-                ys = ys - 1
-                
-                eclaireurs = [blanck[xs + 1, ys], blanck[xs - 1, y], 
-                              blanck[xs, ys + 1], blanck[xs, y - 1],
-                              blanck[xs + 1, ys + 1], blanck[xs - 1, ys - 1],
-                              blanck[xs + 1, ys - 1], blanck[xs - 1, ys + 1]]
-
-                poste = []
-
-                #et a leur tour d'envoyer des éclaireurs
-                copy_six[xs + 1, ys] = 0, 0, 255
-                copy_six[xs - 1, ys] = 0, 0, 255
-                copy_six[xs, ys + 1] = 0, 0, 255
-                copy_six[xs, ys - 1] = 0, 0, 255
-                copy_six[xs + 1, ys + 1] = 0, 0, 255
-                copy_six[xs - 1, ys - 1] = 0, 0, 255
-                copy_six[xs + 1, ys - 1] = 0, 0, 255
-                copy_six[xs - 1, ys + 1] = 0, 0, 255
-
-
-
-                #Routes
-                for nb, eclaireur in enumerate(eclaireurs):
-                    if eclaireur == 255:
-                        print(eclaireur, nb)
-
-                        if nb == 0:poste.append(0)
-                        elif nb == 1:poste.append(1)
-                        elif nb == 2:poste.append(2)
-                        elif nb == 3:poste.append(3)
-                        elif nb == 4:
-                            blanck[xs+1,ys] = 255
-                            poste.append(4)
-                        elif nb == 5:
-                            blanck[xs-1,ys] = 255
-                            poste.append(5)
-                        elif nb == 6:
-                            blanck[xs+1,ys] = 255
-                            poste.append(6)
-                        elif nb == 7:
-                            blanck[xs-1, ys] = 255
-                            poste.append(7)
-
-
-
-
-
-                show_picture("copy_six", copy_six, 0, "")
-
-
-
-
-
-
-
-
-
-
-
-
-
-#copy[x, y] = 0, 0, 255
-#copy[x+1, y] = 0, 255, 0
-#copy[x, y+1] = 255, 0, 0
-
-
-
-
-            if post == 7:
-                print("poste 7")
-
-
-                xse = xse - 1
-                yse = yse + 1
-                
-                eclaireurs = [blanck[xse + 1, yse], blanck[xse - 1, yse], 
-                              blanck[xse, yse + 1], blanck[xse, yse - 1],
-                              blanck[xse + 1, yse + 1], blanck[xse - 1, yse - 1],
-                              blanck[xse + 1, yse - 1], blanck[xse - 1, yse + 1]]
-
-                poste = []
-
-                #et a leur tour d'envoyer des éclaireurs
-                copy_sept[xse + 1, yse] = 0, 0, 255
-                copy_sept[xse - 1, yse] = 0, 0, 255
-                copy_sept[xse, yse + 1] = 0, 0, 255
-                copy_sept[xse, yse - 1] = 0, 0, 255
-                copy_sept[xse + 1, yse + 1] = 0, 0, 255
-                copy_sept[xse - 1, yse - 1] = 0, 0, 255
-                copy_sept[xse + 1, yse - 1] = 0, 0, 255
-                copy_sept[xse - 1, yse + 1] = 0, 0, 255
-
-
-                #Routes
-                for nb, eclaireur in enumerate(eclaireurs):
-                    if eclaireur == 255:
-                        print(eclaireur, nb)
-
-                        if nb == 0:poste.append(0)
-                        elif nb == 1:poste.append(1)
-                        elif nb == 2:poste.append(2)
-                        elif nb == 3:poste.append(3)
-                        elif nb == 4:
-                            blanck[xse+1,yse] = 255
-                            poste.append(4)
-                        elif nb == 5:
-                            blanck[xse-1,yse] = 255
-                            poste.append(5)
-                        elif nb == 6:
-                            blanck[xse+1,yse] = 255
-                            poste.append(6)
-                        elif nb == 7:
-                            blanck[xse-1, yse] = 255
-                            poste.append(7)
-
-
-                show_picture("copy_sept", copy_sept, 0, "")
-
-
+                        lastx = xd
+                        lasty = yd
+
+                cv2.imwrite("coco.png",copy1)
+                cv2.imwrite("dzadzadza.png",blanck)
+                show_picture("copy1", copy1, 0, "")
 
 
 
@@ -647,14 +270,12 @@ while True:
         poste = []
 
         #et a leur tour d'envoyer des éclaireurs
-        copy1[x + i, y] = 0, 0, 255
-        copy1[x - i, y] = 0, 0, 255
-        copy1[x, y + i] = 0, 0, 255
-        copy1[x, y - i] = 0, 0, 255
-        copy1[x + i, y + i] = 0, 0, 255
-        copy1[x - i, y - i] = 0, 0, 255
-        copy1[x + i, y - i] = 0, 0, 255
-        copy1[x - i, y + i] = 0, 0, 255
+
+
+        
+
+        lastx = x
+        lasty = y
 
 
         #Routes
@@ -662,21 +283,28 @@ while True:
             if eclaireur == 255:
                 print(eclaireur, nb)
 
-                if nb == 0:poste.append(0)
-                elif nb == 1:poste.append(1)
-                elif nb == 2:poste.append(2)
-                elif nb == 3:poste.append(3)
+                if nb == 0:
+                    poste.append(0)
+
+                elif nb == 1:
+                    poste.append(1)
+
+                elif nb == 2:
+                    poste.append(2)
+
+                elif nb == 3:
+                    poste.append(3)
+                    
                 elif nb == 4:
-                    blanck[x+1,y] = 255
                     poste.append(4)
+
                 elif nb == 5:
-                    blanck[x-1,y] = 255
                     poste.append(5)
+
                 elif nb == 6:
-                    blanck[x+1,y] = 255
                     poste.append(6)
+ 
                 elif nb == 7:
-                    blanck[x-1, y] = 255
                     poste.append(7)
 
 
