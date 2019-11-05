6418f0b7ba25de3c5c90adc8d9afe9512b81ede6
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-05 14:56:00 +0100

    Update conditions.py
    jy arrive pas ca me dit y'a du blanc mais y'a pas de blanc ptin

--- conditions.py
+++ conditions.py
@@ -1,18 +1,30 @@
 def opposite(liste, nb1, nb2):
+    out = False
 
     try:
-        if liste[-2] == nb1 and\
-            liste[-1] == nb2:
-            print("opposition")
+        for i in range(len(liste)):
+            if liste[i] == nb1 and\
+                liste[i + 1] == nb2:
+                print("opposition")
+                liste.remove(liste[i + 1])
+                out = True
     except IndexError:
         pass
 
+    finally:
+        return out
 
 def corner_bot_right(liste, nb1, nb2):
+    out = False
 
     try:
-        if liste[-3] == nb1 and\
-               liste[-1] == nb2:
+        for i in range(len(liste)):
+            if liste[i] == nb1 and\
+                liste[i+1] == nb2:
                 print("corner bot right")
+                liste.remove(liste[i+1])
+                out = True
     except IndexError:
         pass
+    finally:
+        return out
