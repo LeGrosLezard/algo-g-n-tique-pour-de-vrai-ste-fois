1fc9953e923d08f593d665c849d5dec56c2ac795
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-07 00:21:11 +0100

    Update drawing.py

--- drawing.py
+++ drawing.py
@@ -6,6 +6,7 @@ P = cv2.CHAIN_APPROX_NONE
 
 
 def recup_contours(img, blanck):
+    """Recuperate all contour from a binarized picture"""
 
     edges = cv2.Canny(img, 255,200)
     contours, _ = cv2.findContours(edges, R, P)
@@ -20,6 +21,8 @@ def recup_contours(img, blanck):
 
 
 def incrementation(x, y, number1, number2, copy):
+    """We increment our position if we take descision
+to move on this point"""
 
     x = x + number1
     y = y + number2
@@ -30,6 +33,9 @@ def incrementation(x, y, number1, number2, copy):
 
 
 def neightboors_points(liste):
+    """Here we ask all points  if they're white
+around our position."""
+
 
     name = {"0":"zero", "1":"un", "2": "deux",
             "3":"trois", "4":"quattre",
@@ -42,159 +48,74 @@ def neightboors_points(liste):
                     "sept":0}
 
     for i in liste:
-
         for key, value in name.items():
             if str(i) == key:
                 for key_name, value_name in neightboors.items():
                     if value == key_name:
-                        print("oui")
                         neightboors[key_name] += 1
 
     return neightboors
 
 
 
-def corner(current, copy, blanck, x,  y):
-
-    #   0
-    #   . .
-    #     0
-    #     0
+def no_bloc(last, current):
+    """We search all points around our points.
+    If last move was x + 1, we delete the x - 1 detection
+    Where 1) we are to x we go to x + 1. Detection give us 2)"""
 
 
+    #   x                 x-1
+    #          ->      
+    #  x+1                 x
+
+    #   1)                 2)
 
     
-    zero=0;un=0;deux=0;trois=0;quattre=0;
-    six=0;sept=0;cinq=0;
-
-    for i in current:
-        if i == 0:zero += 1
-        if i == 1:un += 1
-        if i == 2:deux += 1
-        if i == 3:trois += 1
-        if i == 4:quattre += 1
-        if i == 5:cinq += 1
-        if i == 6:six += 1
-        if i == 7:sept += 1
-
-    if deux == 0 and sept == 1 and len(current) == 2:
-        copy[x , y + 1] = 0, 0, 255
-        blanck[x, y + 1] = 255
-        print("cooorner")
-        
-
-    if quattre == 1 and zero == 0 and\
-       len(current) == 2 and cinq == 0:
-        copy[x + 1, y] = 0, 0, 255
-        blanck[x + 1, y] = 255
-        print("cooorner")
-
-    if cinq == 1 and un == 0 and\
-        len(current) <= 2 and quattre == 0:
-        copy[x - 1, y] = 0, 0, 255
-        blanck[x - 1, y] = 255
-        print("cooorner")
-
-
-
-
-
-
-
-def no_bloc(last, current):
-
-
     neightboor = neightboors_points(last)
 
-
-    if neightboor["zero"] == 1:
-        for i in current:
-            if i == 1:
-                current.remove(i)
-    if neightboor["un"] == 1:
-        for i in current:
-            if i == 0:
-                current.remove(i)
-
-    if neightboor["deux"] == 1:
-        for i in current:
-            if i == 3:
-                current.remove(i)
-
-    if neightboor["trois"] == 1:
-        for i in current:
-            if i == 2:
-                current.remove(i)
-
+    if neightboor["zero"] == 1:current.remove(1)
+    if neightboor["un"] == 1:current.remove(0)
+    if neightboor["deux"] == 1:current.remove(3)
+    if neightboor["trois"] == 1:current.remove(2)
     if neightboor["quattre"] == 1:
-        for i in current:
-            if i == 3:
-                current.remove(i)
-
-            if i == 5:
-                current.remove(i)
-
-
+        current.remove(3)
+        current.remove(5)
 
     return current
 
-
-
-    return current
 
 
 
 def diagonale(current):
+    """We delete diagonal. x is our last run.
+We want to go x -> 2 then 4"""
 
-    zero=0;un=0;deux=0;trois=0;quattre=0;
-    six=0;sept=0;cinq=0;quatre_removed=False
-    for i in current:
-        if i == 0:
-            zero += 1
-        if i == 3:
-            trois += 1
+    #   x    x   2
+    #            4  
 
-        if i == 2:
-            deux += 1
-
-        if i == 4:
-            quattre += 1
+    neightboor = neightboors_points(current)
 
 
-    #print(zero,un,deux,trois,quattre)
-
-    if zero == 1 and quattre == 1 :
+    if neightboor["zero"] == 1 and\
+       neightboor["quattre"] == 1 or\
+       neightboor["deux"] == 1 and\
+       neightboor["quattre"] == 1:
         current.remove(4)
-        print("diagolane removed")
-        quatre_removed = True
-
-    if deux == 1 and quattre == 1 and\
-       quatre_removed is False:
-        current.remove(4)
-        print("diagolane removed")
-
-
+        #print("diagonale removed")
 
     return current
 
 
 
 def arrierre_avant(historic, current, x, y, last):
+    """We ask historic and last list if we gone on this
+position.
+Historic recup all points for  the begening, last recup the last
+point."""
 
-    zero=0;un=0;deux=0;trois=0;quattre=0;
-    six=0;sept=0;cinq=0;six_removed = False
+    six_removed = False
 
     if len(historic) >= 3:
-        for i in current:
-            if i == 0:zero += 1
-            if i == 1:un += 1
-            if i == 2:deux += 1
-            if i == 3:trois += 1
-            if i == 4:quattre += 1
-            if i == 5:cinq += 1
-            if i == 6:six += 1
-            if i == 7:sept += 1
-
         if x + 1 == historic[-2][0] and\
            y - 1 == historic[-2][1]:
             current.remove(6)
@@ -205,111 +126,96 @@ def arrierre_avant(historic, current, x, y, last):
             current.remove(7)
 
 
-    
-
+    neightboor = neightboors_points(current)
     if len(last) > 0:
         if last[0] == 7 and\
-            zero == 1 and six == 1:
+            neightboor["zero"] == 1 and\
+            neightboor["six"] == 1:
             current.remove(0)
             current.remove(6)
             six_removed = True
 
         if last[0] == 5 and\
-           deux == 1:
+           neightboor["deux"] == 1:
             current.remove(2)
 
         if last[0] == 2 and\
-           six == 1 and six_removed is False:
+           neightboor["six"] == 1 and\
+           six_removed is False:
             current.remove(6)
 
         if last[0] == 1 and\
-           quattre == 1:
+           neightboor["quattre"] == 1:
             current.remove(4)
-
-
 
     return current
 
 
+
 def corner_to_lign(current, last):
-    
-    zero=0;un=0;deux=0;trois=0;quattre=0;
-    six=0;sept=0;
+    """We remove this"""
+
+
+    #  last
+    #        current                              1)
+    #                 next points
+
+
+
+
+    #  last
+    #          last                                 2)
+    #       to remove   current
+
+
+
+
+    neightboor = neightboors_points(current)
     
     if len(last) > 0:
-        for i in current:
-            if i == 0:zero += 1
-            if i == 1: un += 1
-            if i == 0:trois += 1
-            if i == 1: quattre += 1
-
 
         if last[0] == 7 and\
-           zero == 1 and un == 1:
+           neightboor["zero"] == 1 and\
+           neightboor["un"] == 1:
             current.remove(0)
 
         if last[0] == 6 and\
-           zero == 1 and un == 1:
+           neightboor["zero"] == 1 and\
+           neightboor["un"] == 1:
             current.remove(1) 
 
 
     return current
 
-##
-##                  #0     #1     #2    #3      #4     #5     #6    #7
-##        listex = [x+1,   x-1,   x,    x,      x+1,   x-1,   x+1,  x-1]
-##        listey = [y,       y,   y+1,  y-1,    y+1,   y-1,   y-1,  y+1]
+
 
 def speciale_corner_after_selection(current, copy, blanck, x, y):
+    """Here if we move in diagonal, we complete
+the corner like"""
+
+
+    # last
+    #        current
+    #      to complete   next position
+
+
+
+
     if current[0] == 4:
         copy[x + 1, y]  = 0, 0, 255
         blanck[x + 1, y]  = 255
-        print("special corner 4")
+        #print("special corner 4")
 
     if current[0] == 5:
         copy[x - 1, y]  = 0, 0, 255
         blanck[x - 1, y]  = 255
-        print("special corner 5")
+        #print("special corner 5")
 
 
     if current[0] == 7:
         copy[x , y + 1]  = 0, 0, 255
         blanck[x, y + 1]  = 255
-        print("special corner 7")
-
-
-
-
-
-
-
-
-
-
-
-def thcheck_gray(gray):
-    c = 0
-    for x in range(gray.shape[0]):
-        for y in range(gray.shape[1]):
-            if gray[x, y] == 255:
-                c += 1
-    
-    return c
-
-def thcheck_copy(copy):
-    c1 = 0
-    for x in range(copy.shape[0]):
-        for y in range(copy.shape[1]):
-            if copy[x, y][0] == 0 and\
-               copy[x, y][1] == 0 and\
-               copy[x, y][2] == 255:
-                c1 += 1
- 
-        
-
-    return c1
-
-
+        #print("special corner 7")
 
 
 
