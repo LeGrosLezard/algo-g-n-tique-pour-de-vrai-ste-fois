f6cd1c247b2c8bad8b786beb01b898e152d47889
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-07 23:06:29 +0100

    Update delimitation.py

--- delimitation.py
+++ delimitation.py
@@ -18,26 +18,41 @@ counter = 0
 
 #Openning picture
 img = open_picture('images/lign_v1.jpg')
+#copy = img.copy()
+
+
 
 #Gray and edges filters.
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(img, 255,200)
 
+picture = []
+liste_position = []
+area_list = []
 #Contours are writte into a black picture.
 contours, _ = cv2.findContours(edges, R, P)
 for cnts in contours:
 
     blanck = blanck_picture(img)
     cv2.drawContours(blanck, [cnts], -1, (255,255,255), 1)
+    x, y, w, h = cv2.boundingRect(cnts)
+
+    #cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
+    #show_picture("img", img, 0, "")
+
+    liste_position.append([x, y, w, h])
 
     name = PIT + "contour" + str(counter) + ".png"
-    cv2.imwrite(name, blanck)
+    picture.append(name)
+    area_list.append(cv2.contourArea(cnts))
+    #print(name)
+    #cv2.imwrite(name, blanck)
 
     counter += 1
 
-
-
-
+print(area_list)
+print(picture)
+print(liste_position)
 
 
 
