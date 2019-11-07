98304b0c72d2aca41b76eaa0d85ea519902f9c9d
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-07 17:27:35 +0100

    Update operation.py
    mtn faut si y'a plusoieur lign escalier ect !

--- operation.py
+++ operation.py
@@ -1,88 +1,201 @@
+#for paths
+import os
+
+#Librairy for treating pictures
 import cv2
+from PIL import Image
+
+#Here numpy generate matrix
 import numpy as np
 
+#Show, Open, Create black picture
+from operation import open_picture
+from operation import show_picture
+from operation import blanck_picture
+from operation import find_first_points
+from operation import incrementation
 
-import time
-def open_picture(image):
-    """We open picture for read it."""
-
-    img = cv2.imread(image)
-    return img
-
-
-def show_picture(name, image, mode, destroy):
-    """We Show the picture
-
-        - mode 1 is for an automatic display,
-        - mode 0 wait a press key for destroy picture,
-        -destroy y is for remove picture.
-    """
-
-
-    cv2.imshow(name, image)
-    cv2.waitKey(mode)
-
-    if destroy == "y":
-        cv2.destroyAllWindows()
+from drawing import *
 
 
 
-def save_picture(name, picture):
-    """saving picture we need:
-
-        - his name "".extension,
-        - the picture readed.
-        
-    """
-
-    cv2.imwrite(name, picture)
 
 
 
-def blanck_picture(img):
-    """ Create a black picture bgr, we need:
-
-        - his dimensions (width and height),
-        - his color (0, 0, 0) is blanck default.
-
-    """
-
-    blank_image = np.zeros((img.shape[0],img.shape[1],3), np.uint8)
-    blank_image[0:, 0:] = 0, 0, 0
-
-    return blank_image
 
 
 
-def find_first_points(gray):
-
-
-    pointsx = []  #x pos
-    pointsy = []  #y pos
-
-    #We search the first left top white point.
-    for y in range(gray.shape[1]):
-        for x in range(gray.shape[0]):
-            if gray[x ,y] == 255:
-                pointsx.append([x, y])
-
-    #First left top white point.
-    x = pointsx[0][0]
-    y = pointsx[0][1]
-
-    return x, y
 
 
 
-def incrementation(x, y, number1, number2, copy, mode):
-    """We increment our position if we take descision
-to move on this point"""
 
-    x = x + number1
-    y = y + number2
 
+
+
+path = "images/blanck/"
+
+#We make the treated picture into a list.
+liste_treat = os.listdir(path)
+
+for pict in liste_treat:
+    img = open_picture(path + "contour0blanck.jpg")
+    copy = img.copy()
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    x, y = find_first_points(gray)
+
+
+    last = []
+    suivis = []
+    historic = []
+    pointsy = []
     
-    if mode == "color_copy":
-        copy[x, y]  = 0, 0, 255
+    t = 0
 
-    return x, y
+
+    ocontinuer = True
+    while ocontinuer:
+
+
+                  #0     #1     #2    #3
+        listex = [x+1,   x-1,   x,    x]
+        listey = [y,       y,   y+1,  y-1]
+
+        region = []
+
+        for i in range(len(listex)):
+            if gray[listex[i], listey[i]] > 200:
+                region.append(i)
+
+        if t == 0:
+            region = [region[0]]
+            t += 1
+
+
+        region = no_bloc(last, region)
+
+        for i in region:
+
+            if i == 0:
+                x, y = incrementation(x, y, 1, 0, copy, "")
+                last = []
+                last.append(i)
+                suivis.append(i)
+                copy[x, y] = 0, 0, 255
+                break
+   
+            if i == 1:
+                x, y = incrementation(x, y, -1, 0, copy, "")
+                last = []
+                last.append(i)
+                suivis.append(i)
+                copy[x, y] = 0, 0, 255
+                break
+
+            if i == 2:
+                x, y = incrementation(x, y, 0, 1, copy, "")
+                last = []
+                last.append(i)
+                suivis.append(i)
+                copy[x, y] = 0, 0, 255
+                break
+
+            if i == 3:
+                x, y = incrementation(x, y, 0, -1, copy, "")
+                last = []
+                last.append(i)
+                suivis.append(i)
+                copy[x, y] = 0, 0, 255
+                break
+
+
+
+        historic.append([x, y])
+
+        if len(historic) > 10:
+            if historic[0] == historic[-1]:
+                ocontinuer = False
+                #copy1 = cv2.resize(copy, (800, 800))
+                #show_picture("copy1", copy1, 0, "")
+
+
+
+    print(historic)
+
+    print("")
+    print(suivis)
+
+
+
+    last = 0
+    counter_last = 0
+    schema_lign = []
+
+
+    for nb, i in enumerate(suivis):
+
+        if i == last:
+            counter_last += 1
+
+        elif i != last:
+            if counter_last >= 6:
+                print(counter_last)
+                print(nb)
+                print(suivis[nb - counter_last: nb])
+                print(historic[nb - counter_last: nb])
+
+            counter_last = 0
+
+
+        last = i
+
+
+
+
+
+    b = [[117, 165], [117, 164], [117, 163], [117, 162], [117, 161], [117, 160]]
+
+    for i in b:
+        print(i)
+        copy[i[0], i[1]] = 0, 255, 0
+
+    show_picture("copy", copy, 0, "")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
