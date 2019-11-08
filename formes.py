6ccebded64816c07072faa147965270ba10d73f4
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-08 17:40:51 +0100

    Update formes.py

--- formes.py
+++ formes.py
@@ -20,6 +20,9 @@ from operation import incrementation
 from drawing import *
 
 from display import displaying
+from drawing import *
+
+
 
 def display(dico):
     for cle, valeur in dico.items():
@@ -72,96 +75,208 @@ for i in range(len(liste)):
         if value[0] == i:
             liste_positionnage.append([key, value[1]])
 
+ok_liste = [liste_positionnage[i] for i in range(0, len(liste_positionnage), 2)]
+
+
+points_road = [(33, 60, 37, 69), (60, 109, 56, 95),
+              (83, 109, 89, 108), (140, 95, 134, 84),
+              (159, 107, 160, 117)]
+
+    
+
 
 #on cherche le schema selon la position
-for i in range(0, len(liste_positionnage), 2):
-    try:
-        print(liste_positionnage[i])
-        print(liste_positionnage[i + 2])
+for i in range(len(ok_liste)):
 
-        img = open_picture(liste_positionnage[i][0])
-        img2 = open_picture(liste_positionnage[i + 2][0])
+    print(ok_liste[i])
+    print(ok_liste[i + 1])
 
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = open_picture(ok_liste[i][0])
+    copy = img.copy()
+    img2 = open_picture(ok_liste[i + 1][0])
 
-        (x, y, w, h) = (liste_positionnage[i][1][0],\
-                        liste_positionnage[i][1][1],\
-                        liste_positionnage[i][1][2],\
-                        liste_positionnage[i][1][3])
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-        (x1, y1, w1, h1) = (liste_positionnage[i + 2][1][0],\
-                            liste_positionnage[i + 2][1][1],\
-                            liste_positionnage[i + 2][1][2],\
-                            liste_positionnage[i + 2][1][3])
+    (x, y, w, h) = (ok_liste[i][1][0], ok_liste[i][1][1],\
+                    ok_liste[i][1][2], ok_liste[i][1][3])
+
+    (x1, y1, w1, h1) = (ok_liste[i + 1][1][0], ok_liste[i + 1][1][1],\
+                        ok_liste[i + 1][1][2], ok_liste[i + 1][1][3])
 
 
-        if y + h < y1 + h1:
-            print("celle d'apres est plus basse et a droite")
-        else:
-            print("plus haut et a droite")
-
-
-        print("")
+    if y + h < y1 + h1:
+        print("celle d'apres est plus basse et a droite")
+    else:
+        print("plus haut et a droite")
 
 
 
 
+    y, x = points_road[i][0], points_road[i][1]
+
+    print(x, y)
+    
+    y1, x1 = points_road[i][2], points_road[i][3]
+
+    img[x, y] = 0, 0, 255
+    img[x1, y1] = 255, 0, 0
+
+
+    pbx, pby = find_first_points(gray)
+
+    historic = []
+    last = []
+    t = 0
+
+    ocontinuer = True
+    while ocontinuer:
+
+        #List for decide next move.
+        current = []
+        
+
+
+                  #0     #1     #2    #3      #4     #5     #6    #7
+        listex = [pbx+1,   pbx-1,   pbx,    pbx,      pbx+1,   pbx-1,   pbx+1,  pbx-1]
+        listey = [pby,       pby,   pby+1,  pby-1,    pby+1,   pby-1,   pby-1,  pby+1]
+
+        #We ask white neighboors points.
+        for i in range(len(listex)):
+            if gray[listex[i], listey[i]] >= 250:
+                current.append(i)
 
 
 
+        current = no_bloc(last, current)
+
+        #Delete x - 1 if we have moved to (x + 1; y + 1)
+        current = diagonale(current)
+
+        #Delete from historic
+        current = arrierre_avant(historic, current, pbx, pby, last)
+
+        #Delete a corner by a lign
+        current = corner_to_lign(current, last)
+
+        #Add pixel if we moving by diagonal
+        speciale_corner_after_selection(current, copy, gray, pbx, pby)
 
 
 
-        show_picture("img", img, 0, "")
-        show_picture("img2", img2, 0, "")
-
-
-    except IndexError:
-        pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#display(dico)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
+        historic.append([pbx, pby])
 
 
 
 
 
+        #We decide to begening to move to the bot.
+        if t == 0 and len(current) > 1:
+            current = [current[0]]
+            t += 1
 
+
+
+        for i in current:
+
+            if i == 0:
+                #print("zero")
+                pbx, pby = incrementation(pbx, pby, 1, 0, copy, "color_copy")
+                last = []; last.append(0);
+                copy[pbx, pby]  = 0, 0, 255; break;
+        
+            if i == 1:
+                #print("un")
+                pbx, pby = incrementation(pbx, pby, -1, 0, copy, "color_copy")
+                last = []; last.append(1);
+                copy[pbx, pby]  = 0, 0, 255; break;
+
+            elif i == 2:
+                #print("deux")
+                pbx, pby = incrementation(pbx, pby, 0, 1, copy, "color_copy")
+                last = []; last.append(2);
+                copy[pbx, pby]  = 0, 0, 255; break;
+
+            elif i == 3:
+                #print("trois")
+                pbx, pby = incrementation(pbx, pby, 0, -1, copy, "color_copy")
+                last = []; last.append(3);
+                copy[pbx, pby]  = 0, 0, 255; break;
+
+
+            elif i == 4:
+                #print("quattre")
+                pbx, pby = incrementation(pbx, pby, 1, 1, copy, "color_copy")
+                last = []; last.append(4); break;
+
+            elif i == 5:
+                #print("cinq")
+                pbx, pby = incrementation(pbx, pby, -1, -1, copy, "color_copy")
+                last = []; last.append(5); break;
+
+            elif i == 6:
+                #print("six")
+                pbx, pby = incrementation(pbx, pby, 1, -1, copy, "color_copy")
+                last = []; last.append(6); break;
+
+
+            elif i == 7:
+                #print("sept")
+                pbx, pby = incrementation(pbx, pby, -1, 1, copy, "color_copy")
+                last = []; last.append(7); break;
+
+
+
+
+
+
+        #copy1 = cv2.resize(copy, (200, 200))
+        #show_picture("img", copy1, 0, "")
+
+
+        if len(historic) > 10:
+            if historic[0] == historic[-1]:
+                ocontinuer = False
+
+                for i in historic:
+                    if i[0] == x and i[1] == y:
+                        index = historic.index(i)
+
+                print(len(historic))
+                print("ici", index-10, index+10)
+
+                schema = historic[index-20:index]
+                for i in schema:
+                    copy[i[0], i[1]] = 255, 0, 0
+
+                schema = historic[index:index + 20]
+                for i in schema:
+                    copy[i[0], i[1]] = 0, 255, 0
+
+
+
+                copy1 = cv2.resize(copy, (800, 800))
+                show_picture("img", copy1, 0, "")
+
+
+                img2[x1, y1] = 0, 255, 0
+        
+    
+
+
+
+    
+    copy = cv2.resize(img, (800, 800))
+    copy2 = cv2.resize(img2, (800, 800))
+    cv2.imwrite("ici.png", img)
+    cv2.imwrite("ici2.png", img2)
+    #show_picture("img", copy, 0, "")
+    #show_picture("img2", img2, 0, "")
+
+
+
+    print("")
 
 
 
