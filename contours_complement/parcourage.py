8eb79a392e8c463ca82e70c187a8068730ed2bbb
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-10 18:41:06 +0100

    Update parcourage.py

--- contours_complement/parcourage.py
+++ contours_complement/parcourage.py
@@ -30,9 +30,35 @@ from drawing import arrierre_avant
 from drawing import corner_to_lign
 from drawing import speciale_corner_after_selection
 
+def search_neightboors(current, gray, x, y, t):
+
+    #Neightboor points of the current position.
+
+              #0     #1     #2    #3      #4     #5     #6    #7
+    listex = [x+1,   x-1,   x,    x,      x+1,   x-1,   x+1,  x-1]
+    listey = [y,       y,   y+1,  y-1,    y+1,   y-1,   y-1,  y+1]
 
 
-def choice_next_points(current, last, historic, x, y):
+    #We ask white neighboors points.
+    current = [i for i in range(len(listex))\
+               if gray[listex[i], listey[i]] == 255]
+
+    out = current, ""
+
+    #We decide to begening to move to the bot.
+    if t == 0 and len(current) > 1:
+        current = [current[0]]
+        t += 1
+
+        out = current, t
+
+    return out
+
+
+def choice_next_points(current, last, historic, x, y, copy, gray):
+
+
+
     #Move x + 1 delete x - 1 detection
     current = no_bloc(last, current)
 
@@ -44,8 +70,17 @@ def choice_next_points(current, last, historic, x, y):
 
     #Delete a corner by a lign
     current = corner_to_lign(current, last)
+    
+
+
+    #Add pixel if we moving by diagonal.
+    out = speciale_corner_after_selection(current, copy, gray, x, y)
+    if out != [""]:
+        historic.append([out[0][0], out[0][1]])
+
 
     return current
+
 
 def end_condition(historic, histo_current):
 
@@ -61,6 +96,20 @@ def end_condition(historic, histo_current):
 
     return ocontinuer
 
+
+
+def displaying(gray, copy):
+
+    #Display help part
+    copy1 = copy.copy()
+    #cv2.imwrite("ici.png", copy1)
+    #cv2.imwrite("iciblanck.png", gray)
+    copy1 = cv2.resize(copy1, (800, 800))
+    #show_picture("copy1", copy1, 0, "")
+
+    blanck_resize = cv2.resize(gray, (800, 800))
+    show_picture("copy1", copy1, 1, "")
+    #show_picture("blanck_resize", blanck_resize, 0, "")
 
 
 
@@ -89,103 +138,60 @@ def redraw_contour(pict):
 
         #List for decide next move.
         current = []
-
-        #Neightboor points of the current position.
-
-                  #0     #1     #2    #3      #4     #5     #6    #7
-        listex = [x+1,   x-1,   x,    x,      x+1,   x-1,   x+1,  x-1]
-        listey = [y,       y,   y+1,  y-1,    y+1,   y-1,   y-1,  y+1]
-
-
-        #We ask white neighboors points.
-        for i in range(len(listex)):
-            if gray[listex[i], listey[i]] == 255:
-                current.append(i)
-
-        #We decide to begening to move to the bot.
-        if t == 0 and len(current) > 1:
-            current = [current[0]]
-            t += 1
-
-        #print(last)
-        #print(current)
+        current, t = search_neightboors(current, gray, x, y, t)
 
         #choice of the next point.
-        current = choice_next_points(current, last, historic, x, y)
+        choice_next_points(current, last, historic,
+                           x, y, copy, gray)
 
-        #Add pixel if we moving by diagonal.
-        out = speciale_corner_after_selection(current, copy, gray, x, y)
-        if out != [""]:
-            historic.append([out[0][0], out[0][1]])
-
+        #Recup historic
         historic.append([x, y])
-
-        #print(x, y)
         histo_current.append(current[0])
-
 
 
         #Increment position and Replace last position
         for i in current:
-
+            last = []
             if i == 0:
-                #print("zero")
                 x, y = incrementation(x, y, 1, 0, copy, "color_copy")
-                last = []; last.append(0); break;
+                last.append(0); break;
 
             if i == 1:
-                #print("un")
                 x, y = incrementation(x, y, -1, 0, copy, "color_copy")
-                last = []; last.append(1); break;
+                last.append(1); break;
 
             elif i == 2:
-                #print("deux")
                 x, y = incrementation(x, y, 0, 1, copy, "color_copy")
-                last = []; last.append(2); break;
+                last.append(2); break;
 
             elif i == 3:
-                #print("trois")
                 x, y = incrementation(x, y, 0, -1, copy, "color_copy")
-                last = []; last.append(3); break;
+                last.append(3); break;
 
             elif i == 4:
-                #print("quattre")
                 x, y = incrementation(x, y, 1, 1, copy, "color_copy")
-                last = []; last.append(4); break;
+                last.append(4); break;
 
             elif i == 5:
-                #print("cinq")
                 x, y = incrementation(x, y, -1, -1, copy, "color_copy")
-                last = []; last.append(5); break;
+                last.append(5); break;
 
             elif i == 6:
-                #print("six")
                 x, y = incrementation(x, y, 1, -1, copy, "color_copy")
-                last = []; last.append(6); break;
+                last.append(6); break;
 
 
             elif i == 7:
-                #print("sept")
                 x, y = incrementation(x, y, -1, 1, copy, "color_copy")
-                last = []; last.append(7); break;
+                last.append(7); break;
 
-        #print(x, y)
-        #print("")
 
         #End condition
         ocontinuer = end_condition(historic, histo_current)
-                
 
-        #Display help part
-        copy1 = copy.copy()
-        #cv2.imwrite("ici.png", copy1)
-        #cv2.imwrite("iciblanck.png", gray)
-        copy1 = cv2.resize(copy1, (800, 800))
-        #show_picture("copy1", copy1, 0, "")
+        #Displaying
+        displaying(gray, copy)
 
-        blanck_resize = cv2.resize(gray, (800, 800))
-        show_picture("copy1", copy1, 1, "")
-        #show_picture("blanck_resize", blanck_resize, 0, "")
 
 
     #Saving blanck contour draw
