18ec58356f443aef46d486c062ea76fe9985cf5f
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-10 17:50:20 +0100

    Update delimitation.py

--- starter/delimitation.py
+++ starter/delimitation.py
@@ -1,65 +1,84 @@
+import os
+
+import sys
+sys.path.append(r"C:\Users\jeanbaptiste\Desktop\chaipas")
+
+from paths import path_image_treatment as PIT
+
 #Library
 import cv2
 
 #Opening, Displaying, Creating picture.
-from operation import open_picture
-from operation import show_picture
-from operation import blanck_picture
+from starter.operation import open_picture
+from starter.operation import show_picture
+from starter.operation import blanck_picture
 
-from paths import path_image_treatment as PIT
-
-def crop_contour(pict):
-    #Contours parameters
-    R = cv2.RETR_TREE
-    P = cv2.CHAIN_APPROX_NONE
-
-    #Name variable
-    counter = 0
-
-    #Openning picture
-    img = open_picture(pict)
-    #copy = img.copy()
+#Contours parameters
+R = cv2.RETR_TREE
+P = cv2.CHAIN_APPROX_NONE
 
 
 
+def filters(img):
     #Gray and edges filters.
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(img, 255,200)
+    return edges
 
-    picture = []
-    liste_position = []
-    area_list = []
+
+def create_dico(number, PIT, name):
+
+    dico = {}
+    
+    for i in range(number):
+        dico[name.format(PIT, str(i))] = []
+
+    return dico
+
+
+def add_dico(dico, cnt, number, name, PIT):
+
+
+    for key, value in dico.items():
+        if key == name.format(PIT, str(number)):
+            value.append(cv2.boundingRect(cnt))
+            value.append(cv2.contourArea(cnt))
+                
+    return dico
+
+
+
+def make_contours(pict):
+
+    #Openning picture
+    img = open_picture(pict)
+
+    #filters.
+    edges = filters(img)
+
     #Contours are writte into a black picture.
     contours, _ = cv2.findContours(edges, R, P)
-    for cnts in contours:
+
+    #initialise dico
+    name = "{}contour{}.png"
+    dico_data = create_dico(len(contours), PIT, name)
+
+    for nb, cnt in enumerate(contours):
 
         blanck = blanck_picture(img)
-        cv2.drawContours(blanck, [cnts], -1, (255,255,255), 1)
-        x, y, w, h = cv2.boundingRect(cnts)
+        cv2.drawContours(blanck, [cnt], -1, (255,255,255), 1)
 
-        #cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        dico_data = add_dico(dico_data, cnt, nb, name, PIT)
+
+        cv2.imwrite(name.format(PIT, str(nb)), blanck)
         show_picture("img", img, 0, "")
 
-        liste_position.append([x, y, w, h])
 
-        name = PIT + "contour" + str(counter) + ".png"
-        picture.append(name)
-        area_list.append(cv2.contourArea(cnts))
-        #print(name)
-        cv2.imwrite(name, blanck)
-
-        counter += 1
-
-    print(area_list)
-    print(picture)
-    print(liste_position)
-
-
-
-#crop_contour(r"C:\Users\jeanbaptiste\Desktop\chaipas\images\cercle.jpg")
+    print(dico_data)
+    return dico_data
 
 
 
 
 
-
+#crop_contour(r"C:\Users\jeanbaptiste\Desktop\chaipas\images\lign_v1.jpg")
