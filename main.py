05515464266e8828937933f7fefbc14bb38a8a94
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-10 19:08:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,17 @@ from starter.data_treatment import delete_same_detection
 
 from contours_complement.parcourage import redraw_contour
 
+def put_name(key):
+    name = ""
+    for i in key:
+        try:
+            i = int(i)
+            if i == int(i):
+                name += str(i)
+        except:
+            pass
 
+    return name
 
 #Recuperate contours and save it.
 name = "{}contour{}.png"
@@ -16,10 +26,10 @@ image_path = r"C:\Users\jeanbaptiste\Desktop\chaipas\images\lign_v1.jpg"
 dico_data = make_contours(image_path)
 dico_data = delete_same_detection(dico_data, name, PIT)
 
+
 #Run these contours and rebuild their contours.
 for key, value in dico_data.items():
-    redraw_contour(key)
-    
 
-
+    name = put_name(key)
+    redraw_contour(key, name)
     
