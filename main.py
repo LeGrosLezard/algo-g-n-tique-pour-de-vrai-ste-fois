bc440be2b28621e6e524befa6fc958547902d797
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-12 19:53:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,33 +3,57 @@ import os
 #Path of our pictures
 from paths import path_image_treatment as PIT
 
-from starter.delimitation import make_contours
-from starter.data_treatment import delete_same_detection
 
-from contours_complement.parcourage import redraw_contour
-
-def put_name(key):
-    name = ""
-    for i in key:
-        try:
-            i = int(i)
-            if i == int(i):
-                name += str(i)
-        except:
-            pass
-
-    return name
-
-#Recuperate contours and save it.
 name = "{}contour{}.png"
 image_path = r"C:\Users\jeanbaptiste\Desktop\chaipas\images\lign_v1.jpg"
-dico_data = make_contours(image_path)
-dico_data = delete_same_detection(dico_data, name, PIT)
 
 
-#Run these contours and rebuild their contours.
-for key, value in dico_data.items():
 
-    name = put_name(key)
-    redraw_contour(key, name)
+if __name__ == "__main__":
+
+    #
+    from starter.delimitation import make_contours
+    dico_data = make_contours(image_path)
+
+    #
+    from starter.data_treatment import delete_same_detection
+    dico_data = delete_same_detection(dico_data, name, PIT)
+    #print(dico_data)
+##
+##    #Run these contours and rebuild their contours and recup
+##    #their schemas.
+##    for key, value in dico_data.items():
+##
+##
+##        #
+##        from starter.data_treatment import put_name
+##        name = put_name(key)
+##
+##        #
+##        from contours_complement.parcourage import redraw_contour
+##        historic, histo_current = redraw_contour(key, name)
+##        #print(historic, histo_current)
+
+
+    #Let's show part of pictures.
+    for key, value in dico_data.items():
+        #print(key, value[0])
+        #
+        from data_regroupement.display import displaying
+        displaying(image_path, key, value[0])
+
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
