730b1b723d0c6319999c4bd64c47d13a723b4914
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-12 19:53:49 +0100

    Update paths.py

--- paths.py
+++ paths.py
@@ -1,2 +1,5 @@
+full_path = r"C:\Users\jeanbaptiste\Desktop\chaipas"
 path_image = "images/"
 path_image_treatment = "images/treatment/"
+path_blanck = "images/blanck/{}"
+path_original_picture = "../images/" + "lign_v1.jpg"
