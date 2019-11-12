d8ecba9218e7092c9725fbf3cf543eb56323f15d
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-12 21:25:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,18 @@ if __name__ == "__main__":
     #
     from starter.delimitation import make_contours
     dico_data = make_contours(image_path)
+    #print(dico_data)
 
     #
     from starter.data_treatment import delete_same_detection
     dico_data = delete_same_detection(dico_data, name, PIT)
+    #print(dico_data)
+
+    from starter.data_treatment import sort_forms
+    dico_data = sort_forms(dico_data)
     print(dico_data)
-##
+
+
 ##    #Run these contours and rebuild their contours and recup
 ##    #their schemas.
 ##    for key, value in dico_data.items():
@@ -43,21 +49,22 @@ if __name__ == "__main__":
 ##        displaying(image_path, key, value[0])
 
 
-    from data_regroupement.formes import main_formes
-    data = [[key for key, value in dico_data.items()],
-            [list(value[0]) for key, value in dico_data.items()]]
-
-    #print("\n", data[0], "\n", data[1])
-
-    mini_zone, area_points = main_formes(data[0], data[1], image_path)
+##    from data_regroupement.formes import main_formes
+##    data = [[key for key, value in dico_data.items()],
+##            [list(value[0]) for key, value in dico_data.items()]]
+##
+##    #print("\n", data[0], "\n", data[1])
+##
+##    mini_zone, area_points = main_formes(data[0], data[1], image_path)
+##
+##    print(mini_zone, "\n\n\n", area_points)
 
 
 ##    liste_information = []
 ##    from data_regroupement.points import points_placements
 ##    for key, value in dico_data.items():
 ##        #print(keyn value[0])
-##        liste_information.append([key, [value[0][0], value[0][1],
-##                                        value[0][2], value[0][3]]])
+##        liste_information.append([key, [list(value)]])
 ##    #print(liste_information)
 ##    points_placements(image_path, liste_information)
 
