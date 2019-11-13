834dfed0bb1a75c409c56177dffd6ac36a59becd
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-13 14:35:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
     from starter.data_treatment import sort_forms
     dico_data = sort_forms(dico_data)
-    print(dico_data)
+    #print(dico_data)
 
 
 ##    #Run these contours and rebuild their contours and recup
@@ -49,30 +49,38 @@ if __name__ == "__main__":
 ##        displaying(image_path, key, value[0])
 
 
-##    from data_regroupement.formes import main_formes
-##    data = [[key for key, value in dico_data.items()],
-##            [list(value[0]) for key, value in dico_data.items()]]
-##
-##    #print("\n", data[0], "\n", data[1])
-##
-##    mini_zone, area_points = main_formes(data[0], data[1], image_path)
-##
-##    print(mini_zone, "\n\n\n", area_points)
+    from data_regroupement.formes import main_formes
+    data = [[key for key, value in dico_data.items()],
+            [list(value[0]) for key, value in dico_data.items()]]
+
+    #print("\n", data[0], "\n", data[1])
+
+    mini_zone, area_points = main_formes(data[0], data[1], image_path)
+
+    #print(mini_zone, "\n\n\n", area_points)
 
 
-##    liste_information = []
-##    from data_regroupement.points import points_placements
-##    for key, value in dico_data.items():
-##        #print(keyn value[0])
-##        liste_information.append([key, [list(value)]])
-##    #print(liste_information)
-##    points_placements(image_path, liste_information)
+    liste_information = []
+    from data_regroupement.points import points_placements
+    for key, value in dico_data.items():
+        #print(key, value[0])
+        liste_information.append([key, [list(value)]])
+    #print(liste_information)
+    points, minimum_distance = points_placements(image_path, liste_information)
+    #print("\n", points, "\n\n", minimum_distance)
+
+
+    picture_treat = []
+    for i in data[0]:
+        from starter.data_treatment import put_name
+        name = put_name(i)
+        picture_treat.append("images/blanck/" + name + "blanck.jpg")
 
 
 
-
-
-
+    from reconstruction.connected import main_connected
+    main_connected("reconstruction/ici.png", picture_treat, mini_zone)
+    
 
 
 
